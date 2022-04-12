e1c9b449d26ad9b7948f959dacbc5393cae086c3
Hatsune Miku <hmiku8338@gmail.com>
2022-04-12 09:44:02 -0400

    Major refactoring and addition of model autogeneration

--- e621/api.py
+++ e621/api.py
@@ -1,77 +1,76 @@
-from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
 import requests
+from typing_extensions import TypeAlias
+
+from . import endpoints
+from .models import EnrichedPool, EnrichedPost
+
+Username: TypeAlias = str
+ApiKey: TypeAlias = str
 
 
-class APIRequest:
-    def __init__(self, method, params):
-        self.method = method
-        self.params = params
+class SimpleSession(requests.Session):
+    """A session that automatically configures itself with the necessary auth and headers
+    and always makes requests to base_url
+    """
 
-
-class Endpoint(ABC):
-    def __init__(self, api):
-        self._api = api
-
-
-class PostsEndpoint(Endpoint):
-    pass
-
-
-class FavoritesEndpoint(Endpoint):
-    pass
-
-
-class NotesEndpoint(Endpoint):
-    pass
-
-
-class PoolsEndpoint(Endpoint):
-    pass
-
-
-class RootEndpoint(Endpoint):
-    @property
-    def posts(self):
-        return PostsEndpoint(self._api)
-
-    @property
-    def favorites(self):
-        return FavoritesEndpoint(self._api)
-
-    @property
-    def notes(self):
-        return NotesEndpoint(self._api)
-
-    @property
-    def pools(self):
-        return PoolsEndpoint(self._api)
-
-
-class API:
-    API_URL = "https://e621.net/{endpoint}.json"
-
-    def __new__(cls, *args, **kwargs):
-        api = object.__new__(cls)
-        api.__init__(*args, **kwargs)
-
-        return RootEndpoint(api)
-
-    def __init__(self, login: Optional[str] = None, api_key: Optional[str] = None, timeout = 10):
-        self.login, self.api_key = login, api_key
+    def __init__(self, base_url: str, timeout: int, auth: Optional[Tuple[Username, ApiKey]] = None):
+        super().__init__()
+        self.base_url = base_url
         self.timeout = timeout
+        self.headers.update({"User-Agent": "e621-py (by Eoan Ermine)"})
+        if auth is not None:
+            self.auth = auth
 
-        self.session = requests.Session()
-        self.session.headers = {"User-Agent": "e621-py (by Eoan Ermine)"}
+    def request(self, method: str, endpoint: str, *args: Any, **kwargs: Any) -> requests.Response:
+        url = self.base_url.format(endpoint=endpoint)
+        r = super().request(method, url, *args, timeout=self.timeout, **kwargs)
+        r.raise_for_status()
+        return r
 
-    def send(self, request, http_method: str):
-        request.params["login"] = self.login
-        request.params["api_key"] = self.api_key
 
-        method_url = self.API_URL.format(endpoint=request.method)
-        response = self.session.request(
-            http_method, method_url, params = request.params, timeout = self.timeout
-        )
+class E621API:
+    BASE_URL = "https://e621.net/{endpoint}.json"
 
-        return response.json()
+    def __init__(
+        self,
+        auth: Optional[Tuple[Username, ApiKey]] = None,
+        timeout: int = 10,
+        blacklist: Iterable[str] = (),
+    ) -> None:
+        self.blacklist = set(l.strip() for l in blacklist)
+        self.timeout = timeout
+        self.session = SimpleSession(self.BASE_URL, timeout, auth)
+
+        self.posts = endpoints.Posts(self)
+        self.favorites = endpoints.Favorites(self)
+        self.post_flags = endpoints.PostFlags(self)
+        self.tags = endpoints.Tags(self)
+        self.tag_aliases = endpoints.TagAliases(self)
+        self.notes = endpoints.Notes(self)
+        self.pools = endpoints.Pools(self)
+
+    # Deprecated code. Will be replaced by endpoint classes soon
+
+    def get_posts(self, tags: str, post_count: int = 100000) -> List[EnrichedPost]:
+        params = {"tags": tags, "limit": post_count, "page": 1}
+        all_raw_posts: list[dict[str, Any]] = []
+        # TODO: Replace me with walrus when it's available :(
+        raw_posts = self.session.get("posts", params).json()["posts"]
+        while len(raw_posts) and post_count - len(raw_posts) >= 0:
+            params["page"] += 1
+            params["limit"] -= len(raw_posts)
+            all_raw_posts.extend(raw_posts)
+            # TODO: Replace me with walrus when it's available :(
+            raw_posts = self.session.get("posts", params).json()["posts"]
+        posts = (EnrichedPost(**p) for p in all_raw_posts)
+        # FIXME: this works with single tag on the blacklist but e621 supports tag groups in blacklists
+        # FIXME: this works if the person put the tag correctly, but doesn't work with tag aliases
+        return [p for p in posts if not p.all_tags & self.blacklist]
+
+    def get_post(self, post_id: int) -> EnrichedPost:
+        return EnrichedPost(**self.session.get(f"posts/{post_id}").json())
+
+    def get_pool(self, pool_id: int) -> EnrichedPool:
+        return EnrichedPool(**self.session.get(f"pools/{pool_id}").json(), _e6api=self)
--- e621/e621.py
+++ e621/e621.py
@@ -20,17 +20,21 @@ class Flag:
         return self._api.request(
             "post_flags",
             search={
-                "post_id": post_id, "creator_id": creator_id, "creator_name": creator_name
-            }
+                "post_id": post_id,
+                "creator_id": creator_id,
+                "creator_name": creator_name,
+            },
         )
 
     def create(self, post_id, reason_name, parent_id=None):
         return self._api.request(
             "post_flags",
             post_flags={
-                "post_id": post_id, "reason_name": reason_name, "parent_id": parent_id
+                "post_id": post_id,
+                "reason_name": reason_name,
+                "parent_id": parent_id,
             },
-            type="POST"
+            type="POST",
         )
 
 
@@ -38,42 +42,76 @@ class Posts:
     def __init__(self, api: e621):
         self._api = api
 
-    def create(self, tag_string, file, rating, source, direct_url=None, description=None, parent_id=None,
-               referer_url=None, md5_confirmation=None, as_pending=None):
+    def create(
+        self,
+        tag_string,
+        file,
+        rating,
+        source,
+        direct_url=None,
+        description=None,
+        parent_id=None,
+        referer_url=None,
+        md5_confirmation=None,
+        as_pending=None,
+    ):
         return self._api.request(
             "uploads",
             upload={
-                "tag_string": tag_string, "file": file, "rating": rating, "source": source,
-                "direct_url": direct_url, "description": description, "parent_id": parent_id,
-                "referer_url": referer_url, "md5_confirmation": md5_confirmation, "as_pending": as_pending
+                "tag_string": tag_string,
+                "file": file,
+                "rating": rating,
+                "source": source,
+                "direct_url": direct_url,
+                "description": description,
+                "parent_id": parent_id,
+                "referer_url": referer_url,
+                "md5_confirmation": md5_confirmation,
+                "as_pending": as_pending,
             },
-            type="POST"
+            type="POST",
         )
 
-    def update(self, post_id: int, tag_string_diff=None, source_diff=None, parent_id=None, old_parent_id=None,
-               description=None, old_description=None, rating=None, old_rating=None, is_rating_locked=None,
-               is_note_locked=None, edit_reason=None, has_embedded_notes=None):
+    def update(
+        self,
+        post_id: int,
+        tag_string_diff=None,
+        source_diff=None,
+        parent_id=None,
+        old_parent_id=None,
+        description=None,
+        old_description=None,
+        rating=None,
+        old_rating=None,
+        is_rating_locked=None,
+        is_note_locked=None,
+        edit_reason=None,
+        has_embedded_notes=None,
+    ):
         return self._api.request(
             f"posts/{post_id}",
             post={
-                "tag_string_diff": tag_string_diff, "source_diff": source_diff, "parent_id": parent_id,
-                "old_parent_id": old_parent_id, "description": description, "old_description": old_description,
-                "rating": rating, "old_rating": old_rating, "is_rating_locked": is_rating_locked,
-                "is_note_locked": is_note_locked, "edit_reason": edit_reason, "has_embedded_notes": has_embedded_notes
+                "tag_string_diff": tag_string_diff,
+                "source_diff": source_diff,
+                "parent_id": parent_id,
+                "old_parent_id": old_parent_id,
+                "description": description,
+                "old_description": old_description,
+                "rating": rating,
+                "old_rating": old_rating,
+                "is_rating_locked": is_rating_locked,
+                "is_note_locked": is_note_locked,
+                "edit_reason": edit_reason,
+                "has_embedded_notes": has_embedded_notes,
             },
-            type="PATCH"
+            type="PATCH",
         )
 
     def list(self, limit: int, tags: str, page: int):
-        return self._api.request(
-            "posts", limit=limit, tags=tags, page=page
-        )
+        return self._api.request("posts", limit=limit, tags=tags, page=page)
 
     def vote(self, post_id, score, no_unvote=None):
-        return self._api.request(
-            f"posts/{post_id}/votes", score=score, no_unvote=no_unvote,
-            type="POST"
-        )
+        return self._api.request(f"posts/{post_id}/votes", score=score, no_unvote=no_unvote, type="POST")
 
     @property
     def flag(self):
@@ -84,16 +122,29 @@ class Tags:
     def __init__(self, api: e621):
         self._api = api
 
-    def list(self, name_matches, page, category=None, order=None, hide_empty=None, has_wiki=None,
-             has_artist=None, limit=None):
+    def list(
+        self,
+        name_matches,
+        page,
+        category=None,
+        order=None,
+        hide_empty=None,
+        has_wiki=None,
+        has_artist=None,
+        limit=None,
+    ):
         return self._api.request(
             "tags",
             search={
-                "name_matches": name_matches, "category": category, "order": order,
-                "hide_empty": hide_empty, "has_wiki": has_wiki, "has_artists": has_artist,
+                "name_matches": name_matches,
+                "category": category,
+                "order": order,
+                "hide_empty": hide_empty,
+                "has_wiki": has_wiki,
+                "has_artists": has_artist,
             },
             limit=limit,
-            page=page
+            page=page,
         )
 
 
@@ -101,12 +152,22 @@ class TagAliases:
     def __init__(self, api: e621):
         self._api = api
 
-    def list(self, name_matches, page, antecedent_tag_category, consequent_tag_category, status=None, order=None,
-             limit=None):
+    def list(
+        self,
+        name_matches,
+        page,
+        antecedent_tag_category,
+        consequent_tag_category,
+        status=None,
+        order=None,
+        limit=None,
+    ):
         return self._api.request(
             "tag_aliases",
             search={
-                "name_matches": name_matches, "status": status, "order": order,
+                "name_matches": name_matches,
+                "status": status,
+                "order": order,
                 "antecedent_tag": {"category": antecedent_tag_category},
                 "consequent_tag": {"category": consequent_tag_category},
             },
@@ -119,12 +180,25 @@ class Notes:
     def __init__(self, api: e621):
         self._api = api
 
-    def list(self, body_matches, post_id, post_tags_match, creator_name, creator_id, is_active, limit):
+    def list(
+        self,
+        body_matches,
+        post_id,
+        post_tags_match,
+        creator_name,
+        creator_id,
+        is_active,
+        limit,
+    ):
         return self._api.request(
             "notes",
             search={
-                "body_matches": body_matches, "post_id": post_id, "post_tags_match": post_tags_match,
-                "creator_name": creator_name, "creator_id": creator_id, "is_active": is_active
+                "body_matches": body_matches,
+                "post_id": post_id,
+                "post_tags_match": post_tags_match,
+                "creator_name": creator_name,
+                "creator_id": creator_id,
+                "is_active": is_active,
             },
             limit=limit,
         )
@@ -133,81 +207,104 @@ class Notes:
         return self._api.request(
             "notes",
             note={
-                "post_id": post_id, "x": x, "y": y, "width": width, "height": height,
-                "body": body
+                "post_id": post_id,
+                "x": x,
+                "y": y,
+                "width": width,
+                "height": height,
+                "body": body,
             },
-            type="POST"
+            type="POST",
         )
 
     def update(self, note_id, x, y, width, height, body):
         return self._api.request(
             f"notes/{note_id}",
-            note={
-                "x": x, "y": y, "width": width, "height": height, "body": body
-            },
-            type="PUT"
+            note={"x": x, "y": y, "width": width, "height": height, "body": body},
+            type="PUT",
         )
 
     def delete(self, note_id):
-        return self._api.request(
-            f"notes/{note_id}",
-            type="DELETE"
-        )
+        return self._api.request(f"notes/{note_id}", type="DELETE")
 
     def revert(self, note_id, version_id):
-        return self._api.request(
-            f"notes/{note_id}/",
-            version_id=version_id,
-            type="PUT"
-        )
+        return self._api.request(f"notes/{note_id}/", version_id=version_id, type="PUT")
 
 
 class Pools:
     def __init__(self, api: e621):
         self._api = api
 
-    def list(self, name_matches, is_active, is_deleted, category, limit,
-             id=None, description_matches=None, creator_name=None, creator_id=None, order=None):
+    def list(
+        self,
+        name_matches,
+        is_active,
+        is_deleted,
+        category,
+        limit,
+        id=None,
+        description_matches=None,
+        creator_name=None,
+        creator_id=None,
+        order=None,
+    ):
 
         if not all([id, description_matches, creator_name, creator_id]):
-            raise RuntimeError("One of arguments should be passed: id, description_matches, creator_name,"
-                               " creator_id")
+            raise RuntimeError(
+                "One of arguments should be passed: id, description_matches, creator_name," " creator_id"
+            )
 
         return self._api.request(
             "pools",
             search={
-                "name_matches": name_matches, "id": id, "description_matches": description_matches, "creator_name":
-                creator_name, "creator_id": creator_id, "is_active": is_active, "is_deleted": is_deleted, "category":
-                category, "order": order
+                "name_matches": name_matches,
+                "id": id,
+                "description_matches": description_matches,
+                "creator_name": creator_name,
+                "creator_id": creator_id,
+                "is_active": is_active,
+                "is_deleted": is_deleted,
+                "category": category,
+                "order": order,
             },
-            limit=limit
+            limit=limit,
         )
 
     def create(self, name, description, category=None, is_locked=None):
         return self._api.request(
             "pools",
             pool={
-                "name": name, "description": description, "category": category, "is_locked": is_locked
+                "name": name,
+                "description": description,
+                "category": category,
+                "is_locked": is_locked,
             },
-            type="POST"
+            type="POST",
         )
 
-    def update(self, pool_id, name=None, description=None, post_ids=None, is_active=None, category=None):
+    def update(
+        self,
+        pool_id,
+        name=None,
+        description=None,
+        post_ids=None,
+        is_active=None,
+        category=None,
+    ):
         return self._api.request(
             f"pools/{pool_id}",
             pool={
-                "name": name, "description": description, "post_ids": post_ids, "is_active": is_active, "category":
-                category
+                "name": name,
+                "description": description,
+                "post_ids": post_ids,
+                "is_active": is_active,
+                "category": category,
             },
-            type="PUT"
+            type="PUT",
         )
 
     def revert(self, pool_id, version_id):
-        return self._api.request(
-            f"pools/{pool_id}",
-            version_id=version_id,
-            type="PUT"
-        )
+        return self._api.request(f"pools/{pool_id}", version_id=version_id, type="PUT")
 
 
 class e621:
@@ -215,38 +312,20 @@ class e621:
         self._login = login
         self._api_key = api_key
 
-        self._credentials = {
-            "login": login, "api_key": api_key
-        }
+        self._credentials = {"login": login, "api_key": api_key}
 
     def request(self, method: str, type="GET", **kwargs):
         url = get_url(method)
         kwargs.update(self._credentials)
 
         if type == "GET":
-            return requests.get(
-                url,
-                params=kwargs,
-                headers=HEADERS
-            ).json()
+            return requests.get(url, params=kwargs, headers=HEADERS).json()
         elif type == "POST":
-            return requests.post(
-                url,
-                params=kwargs,
-                headers=HEADERS
-            ).json()
+            return requests.post(url, params=kwargs, headers=HEADERS).json()
         elif type == "PATCH":
-            return requests.patch(
-                url,
-                params=kwargs,
-                headers=HEADERS
-            ).json()
+            return requests.patch(url, params=kwargs, headers=HEADERS).json()
         elif type == "PUT":
-            return requests.put(
-                url,
-                params=kwargs,
-                headers=HEADERS
-            )
+            return requests.put(url, params=kwargs, headers=HEADERS)
 
     @property
     def posts(self):
